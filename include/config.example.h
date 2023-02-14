26e02eec41341cfbb27a0cf0891647a0a88ae039
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2023-02-14 14:02:26 +0100

    Rename SMP_BRIDGE_AP to SMP_AP

--- include/config.example.h
+++ include/config.example.h
@@ -1,9 +1,9 @@
 #ifndef SMP_CONFIG_
 #define SMP_CONFIG_
 
-#define SMP_BRIDGE_AP_SSID     "the-awesome-bridge"
-#define SMP_BRIDGE_AP_PASSWORD "my-fucking-password"
-#define SMP_BRIDGE_IP_ADDRESS  "192.168.1.1"
-#define SMP_BRIDGE_PORT        2323
+#define SMP_AP_SSID           "the-awesome-bridge"
+#define SMP_AP_PASSWORD       "my-fucking-password"
+#define SMP_BRIDGE_IP_ADDRESS "192.168.1.1"
+#define SMP_BRIDGE_PORT       2323
 
 #endif // SMP_CONFIG_
